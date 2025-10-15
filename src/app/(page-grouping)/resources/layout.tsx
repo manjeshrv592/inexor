0d23c4587f2d3bddce2d6d09c16d722cc74af9ef
manjesh <manjeshrv592@gmail.com>
2025-10-16 02:33:45 +0530

    Populated back page panel

--- src/app/(page-grouping)/resources/layout.tsx
+++ src/app/(page-grouping)/resources/layout.tsx
@@ -2,13 +2,11 @@ import PagePanel from "@/components/ui/PagePanel";
 import PagePanelBg from "@/components/ui/PagePanelBg";
 import React from "react";
 
-// const ResourcesLayout = ({ children }: { children: React.ReactNode }) => {
-const ResourcesLayout = () => {
+const ResourcesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <PagePanelBg />
-      {/* <PagePanel>{children}</PagePanel> */}
-      <PagePanel>{"Page Panel"}</PagePanel>
+      <PagePanel>{children}</PagePanel>
     </>
   );
 };
