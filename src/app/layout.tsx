193fc90802292f6f9a8b3ca6f1f1667dea17b752
manjesh <manjeshrv592@gmail.com>
2025-10-16 12:29:17 +0530

    Added back rest of the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,19 +6,18 @@ import "./globals.css";
 import Header from "@/components/layout/header/Header";
 import AuthProvider from "@/components/providers/AuthProvider";
 import LogoutButton from "@/components/auth/LogoutButton";
-// import Footer from "@/components/layout/Footer";
-// import PrefetchProvider from "@/components/providers/PrefetchProvider";
-// import AboutOverview from "@/components/sections/about-overview/AboutOverview";
-// import Clients from "@/components/sections/Clients";
+import Footer from "@/components/layout/Footer";
+import PrefetchProvider from "@/components/providers/PrefetchProvider";
+import AboutOverview from "@/components/sections/about-overview/AboutOverview";
+import Clients from "@/components/sections/Clients";
 import Hero from "@/components/sections/Hero";
 import { getStaticHomepageData } from "@/lib/static-generation";
-// import KeyValuePillars from "@/components/sections/KeyValuePillars";
-// import Maps from "@/components/sections/Maps";
-// import OurServices from "@/components/sections/OurServices";
-// import Testimonials from "@/components/sections/Testimonials";
-// import WhoWeServe from "@/components/sections/WhoWeServe";
-// import Why from "@/components/sections/Why";
-// import { getStaticHomepageData } from "@/lib/static-generation";
+import KeyValuePillars from "@/components/sections/KeyValuePillars";
+import Maps from "@/components/sections/Maps";
+import OurServices from "@/components/sections/OurServices";
+import Testimonials from "@/components/sections/Testimonials";
+import WhoWeServe from "@/components/sections/WhoWeServe";
+import Why from "@/components/sections/Why";
 
 const michroma = Michroma({
   subsets: ["latin"],
@@ -42,23 +41,21 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const {
-  //   heroData,
-  //   keyValuePillarsSection,
-  //   keyValuePillarItems,
-  //   aboutData,
-  //   aboutItems,
-  //   whoWeServeItems,
-  //   whoWeServeSection,
-  //   whyData,
-  //   whyItems,
-  //   servicesSection,
-  //   serviceItems,
-  //   testimonialsData,
-  //   footerData,
-  // } = await getStaticHomepageData();
-
-  const { heroData } = await getStaticHomepageData();
+  const {
+    heroData,
+    keyValuePillarsSection,
+    keyValuePillarItems,
+    aboutData,
+    aboutItems,
+    whoWeServeItems,
+    whoWeServeSection,
+    whyData,
+    whyItems,
+    servicesSection,
+    serviceItems,
+    testimonialsData,
+    footerData,
+  } = await getStaticHomepageData();
 
   return (
     <ViewTransitions>
@@ -67,33 +64,33 @@ export default async function RootLayout({
           className={`${michroma.variable} ${raleway.variable} bg-[#050505] text-white antialiased`}
         >
           <AuthProvider>
-            {/* {<PrefetchProvider>} */}
-            <Header />
-            <main>
-              <Hero heroData={heroData} />
-              {/* <KeyValuePillars
-                sectionData={keyValuePillarsSection}
-                items={keyValuePillarItems}
-              />
-              <AboutOverview aboutData={aboutData} aboutItems={aboutItems} />
-              <WhoWeServe
-                items={whoWeServeItems}
-                sectionData={whoWeServeSection}
-              />
+            <PrefetchProvider>
+              <Header />
+              <main>
+                <Hero heroData={heroData} />
+                <KeyValuePillars
+                  sectionData={keyValuePillarsSection}
+                  items={keyValuePillarItems}
+                />
+                <AboutOverview aboutData={aboutData} aboutItems={aboutItems} />
+                <WhoWeServe
+                  items={whoWeServeItems}
+                  sectionData={whoWeServeSection}
+                />
 
-              <OurServices
-                servicesSection={servicesSection}
-                serviceItems={[...serviceItems].reverse()}
-              />
-              <Maps />
-              <Why whyData={whyData} whyItems={whyItems} />
-              <Clients />
-              <Testimonials testimonialsData={testimonialsData} />
-              <Footer footerData={footerData} /> */}
-            </main>
-            {children}
-            <LogoutButton />
-            {/* {  </PrefetchProvider>} */}
+                <OurServices
+                  servicesSection={servicesSection}
+                  serviceItems={[...serviceItems].reverse()}
+                />
+                <Maps />
+                <Why whyData={whyData} whyItems={whyItems} />
+                <Clients />
+                <Testimonials testimonialsData={testimonialsData} />
+                <Footer footerData={footerData} />
+              </main>
+              {children}
+              <LogoutButton />
+            </PrefetchProvider>
             <Toaster
               position="top-right"
               toastOptions={{
